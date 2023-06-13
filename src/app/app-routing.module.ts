9184398706df68fe5d4122e8f743356a0e4d6732
QuentinFraise <fraisequentingit@gmail.com>
2023-06-13 20:32:47 +0200

    Structure Portfolio fonctionnel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcceuilComponent } from './component/acceuil/acceuil.component';
 import { AppComponent } from './app.component';
+import { AcceuilModule } from './component/acceuil/acceuil.module';
 
-const routes: Routes = [
-  { path: '', component: AppComponent,
-children:[
-  {
-    path:'',
-    loadChildren:() => import('./component/acceuil/acceuil.module').then(m => m.AcceuilModule)
-  }
-] },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
